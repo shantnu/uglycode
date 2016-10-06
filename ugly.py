63048f1063291f5f6d51925a9e15d1ae23eea387
Shantnu Tiwari <shantnu@users.noreply.github.com>
2016-10-06 16:54:56 +0100

    100% PEP8 compliant now!

--- ugly.py
+++ ugly.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python
 
 # Read user name
-a=input("Enter name: ")
+a = input("Enter name: ")
 
 # read user age
-b=input("Enter age: ")
+b = input("Enter age: ")
 
-#get marks for subject 1
-m1=input("Enter marks for subject 1 ")
+# get marks for subject 1
+m1 = input("Enter marks for subject 1 ")
 
-#get marks for subject 1
-m2=input("Enter marks for subject 1 ")
+# get marks for subject 1
+m2 = input("Enter marks for subject 1 ")
 
-#get marks for subject 1
-m3=input("Enter marks for subject 1 ")
+# get marks for subject 1
+m3 = input("Enter marks for subject 1 ")
 
 # convert to int
 m4 = int(m1)
@@ -23,9 +23,9 @@ m6 = int(m3)
 # Find total
 c = m4 + m5 + m6
 
-#find average
+# find average
 d = c / 3
 
-#print the total and average
-print("Your total marks",c)
-print("Your average marks",d)
+# print the total and average
+print("Your total marks", c)
+print("Your average marks", d)
